6c0f8e2c3dbc08cf4c55eed7e9fb93283a9a3776
Syed Waqee <waqee15@gmail.com>
2019-12-15 23:27:55 +0000

    check for files to compare exist + writing error to log

--- org.alloytools.alloy.diff/src/main/java/edu/mit/csail/sdg/alloy4whole/CompareFilesDialog.java
+++ org.alloytools.alloy.diff/src/main/java/edu/mit/csail/sdg/alloy4whole/CompareFilesDialog.java
@@ -1,8 +1,8 @@
 package edu.mit.csail.sdg.alloy4whole;
 
 import java.awt.Component;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -26,282 +26,234 @@ import javax.swing.SwingUtilities;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.plaf.basic.BasicComboBoxRenderer;
+import javax.swing.text.StyledEditorKit.FontSizeAction;
 
 import org.alloytools.alloy.diff.ModuleMerger;
 
 import edu.mit.csail.sdg.alloy4.A4Preferences.ChoicePref;
 import edu.mit.csail.sdg.alloy4.A4Preferences.Pref;
 import edu.mit.csail.sdg.alloy4.A4Preferences.StringChoicePref;
-import edu.mit.csail.sdg.alloy4.OurBorder;
+import edu.mit.csail.sdg.alloy4.OurDialog;
 import edu.mit.csail.sdg.alloy4.OurSyntaxWidget;
 import edu.mit.csail.sdg.alloy4.OurTabbedSyntaxWidget;
 import edu.mit.csail.sdg.alloy4.OurUtil;
 import edu.mit.csail.sdg.alloy4.OurUtil.GridBagConstraintsBuilder;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.AnalyzerHeight;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.AnalyzerWidth;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.AnalyzerX;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.AnalyzerY;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.AntiAlias;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.AutoVisualize;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.CoreGranularity;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.CoreMinimization;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.FontName;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.FontSize;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.ImplicitThis;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.InferPartialInstance;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.LAF;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.Model0;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.Model1;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.Model2;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.Model3;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.NoOverflow;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.RecordKodkod;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.SkolemDepth;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.Solver;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.SubMemory;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.SubStack;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.SyntaxDisabled;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.TabSize;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.Unrolls;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.VerbosityPref;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.WarningNonfatal;
+import static edu.mit.csail.sdg.alloy4.A4Preferences.Welcome;
 
-@SuppressWarnings({
-                   "serial"
-} )
+
+@SuppressWarnings({ "serial" })
 public class CompareFilesDialog extends JFrame {
 
-    private JEditorPane tab;
+	@SuppressWarnings("unchecked")
+	private static class CBModel<T> extends AbstractListModel implements ComboBoxModel {
 
-    @SuppressWarnings("unchecked" )
-    private static class CBModel<T> extends AbstractListModel implements ComboBoxModel {
+		private final ChoicePref<T> pref;
 
-        private final ChoicePref<T> pref;
+		public CBModel(final ChoicePref<T> pref) {
+			this.pref = pref;
+			this.pref.addChangeListener(new ChangeListener() {
 
-        public CBModel(final ChoicePref<T> pref) {
-            this.pref = pref;
-            this.pref.addChangeListener(new ChangeListener() {
+				@Override
+				public void stateChanged(ChangeEvent e) {
+					fireContentsChanged(pref, -1, -1);
+				}
+			});
+		}
 
-                @Override
-                public void stateChanged(ChangeEvent e) {
-                    fireContentsChanged(pref, -1, -1);
-                }
-            });
-        }
+		@Override
+		public int getSize() {
+			return pref.validChoices().size();
+		}
 
-        @Override
-        public int getSize() {
-            return pref.validChoices().size();
-        }
+		@Override
+		public Object getElementAt(int index) {
+			return pref.validChoices().get(index);
+		}
 
-        @Override
-        public Object getElementAt(int index) {
-            return pref.validChoices().get(index);
-        }
+		@Override
+		public void setSelectedItem(Object anItem) {
+			pref.set((T) anItem);
+		}
 
-        @Override
-        public void setSelectedItem(Object anItem) {
-            pref.set((T) anItem);
-        }
+		@Override
+		public Object getSelectedItem() {
+			return pref.get();
+		}
+	}
 
-        @Override
-        public Object getSelectedItem() {
-            return pref.get();
-        }
-    }
+	private abstract class CBRenderer extends BasicComboBoxRenderer {
 
-    private OurTabbedSyntaxWidget text;
+		@Override
+		public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected,
+				boolean cellHasFocus) {
+			return super.getListCellRendererComponent(list, render(value), index, isSelected, cellHasFocus);
+		}
 
-    private static class BRModel<T> implements BoundedRangeModel {
+		protected abstract Object render(Object value);
+	}
 
-        private final ChoicePref<T> pref;
+	private final Map<Pref<?>, JComponent> pref2comp = new HashMap<Pref<?>, JComponent>();
+	private final String binary;
+	private final String currentViewFile;
+	private final SwingLogPanel log;
 
-        public BRModel(ChoicePref<T> pref) {
-            this.pref = pref;
-        }
+	private List<OurSyntaxWidget> tabsList;
 
-        @Override
-        public int getMinimum() {
-            return 0;
-        }
+	public CompareFilesDialog(SwingLogPanel log, String binary, List<OurSyntaxWidget> list, String currentViewFile) {
+		this.log = log;
+		this.binary = binary;
+		this.tabsList = list;
+		this.currentViewFile = currentViewFile;
+		if (log != null && binary != null) {
+		}
+		initUI();
+	}
 
-        @Override
-        public int getMaximum() {
-            return pref.validChoices().size() - 1;
-        }
+	protected final void initUI() {
 
-        @Override
-        public int getValue() {
-            return pref.getSelectedIndex();
-        }
+		JLabel dialogLabel = new JLabel();
+		dialogLabel.setText("Find instances of Right file that are not instances of Left file");
+		//dialogLabel.setFont(new Font("Courier", Font.BOLD,12));
+		
+		ArrayList<String> tabListNames = new ArrayList<String>();
+		for (OurSyntaxWidget tab : tabsList) {
+			//tabListNames.add(tab.getFilename());
+			tabListNames.add(tab.getFilename().substring(tab.getFilename().lastIndexOf('\\') + 1));
+		}
 
-        @Override
-        public int getExtent() {
-            return 0;
-        }
+		StringChoicePref tabNamesLeft = new StringChoicePref("LeftFile", "Left File", tabListNames);
+		StringChoicePref tabNamesRight = new StringChoicePref("RightFile", "Right File", tabListNames);
 
-        @Override
-        public void setValueIsAdjusting(boolean b) {
-        }
+		JButton compareButton = new JButton("Compare");
 
-        @Override
-        public boolean getValueIsAdjusting() {
-            return false;
-        }
+		tabNamesLeft.set(currentViewFile);
+		tabNamesRight.setSelectedIndex(0);
 
-        @Override
-        public void setRangeProperties(int value, int extent, int min, int max, boolean adjusting) {
-            throw new UnsupportedOperationException();
-        }
+		JPanel p = OurUtil.makeGrid(2, gbc().make(), mkCombo(tabNamesLeft),
+				mkCombo(tabNamesRight), mkButton(compareButton));
 
-        @Override
-        public void addChangeListener(ChangeListener x) {
-            pref.addChangeListener(x);
-        }
+		new JEditorPane();
 
-        @Override
-        public void removeChangeListener(ChangeListener x) {
-            pref.removeChangeListener(x);
-        }
+		add(p);
+		//add(dialogLabel);
+		// add(mkCombo(tabNamesRight));
 
-        @Override
-        public void setValue(int n) {
-            if (n >= getMinimum() && n <= getMaximum())
-                pref.setSelectedIndex(n);
-        }
+		setTitle("Compare Alloy Models");
+		
+		pack();
+		setSize(getSize().width + 5, getSize().height + 5);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setAlwaysOnTop(false);
 
-        @Override
-        public void setExtent(int n) {
-            throw new UnsupportedOperationException();
-        }
+		compareButton.addActionListener(new ActionListener() {
 
-        @Override
-        public void setMinimum(int n) {
-            throw new UnsupportedOperationException();
-        }
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ModuleMerger m = new ModuleMerger(tabNamesLeft.get(), tabNamesRight.get());
+				// dispose();
+			}
+		});
+	}
 
-        @Override
-        public void setMaximum(int n) {
-            throw new UnsupportedOperationException();
-        }
-    }
+	@SuppressWarnings({ "unchecked" })
+	protected <T> JPanel mkCombo(final ChoicePref<T> pref) {
+		JComboBox cb = make(new JComboBox(mkComboBoxModelFor(pref)));
+		pref2comp.put(pref, cb);
+		cb.setRenderer(new CBRenderer() {
 
-    private abstract class CBRenderer extends BasicComboBoxRenderer {
+			@Override
+			protected Object render(Object value) {
+				return pref.renderValueShort((T) value);
+			}
+		});
+		return OurUtil.makeH(pref.title + ": ", cb);
+	}
 
-        @Override
-        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-            return super.getListCellRendererComponent(list, render(value), index, isSelected, cellHasFocus);
-        }
+	protected <T> JPanel mkButton(JButton jb) {
+		return OurUtil.makeH(null, jb, null);
+	}
+	
+	protected <T> JPanel mkLabel(JLabel l) {
+		return OurUtil.makeH(null, l, null);
+	}
+	
+	private <T> ComboBoxModel mkComboBoxModelFor(ChoicePref<T> pref) {
+		return new CBModel<T>(pref);
+	}
 
-        protected abstract Object render(Object value);
-    }
+	private <T extends JComponent> T make(T comp) {
+		return OurUtil.make(comp);
+	}
 
-    private final Map<Pref< ? >,JComponent> pref2comp = new HashMap<Pref< ? >,JComponent>();
-    private final String                    binary;
-    private final SwingLogPanel             log;
+	private GridBagConstraintsBuilder gbc() {
+		GridBagConstraintsBuilder ans = new GridBagConstraintsBuilder();
+		ans.anchor(GridBagConstraints.WEST).insets(new Insets(3, 3, 3, 3)).ipads(3, 3).fill(GridBagConstraints.BOTH);
+		return ans;
+	}
 
-    private List<OurSyntaxWidget>           tabsList;
+	public static void logPrefChanged(SwingLogPanel log, Pref<?> pref) {
+		if (log == null)
+			return;
+		log.log("Option ");
+		log.logBold(pref.title);
+		log.log(" changed to ");
+		log.logBold(pref.get() + "\n");
+		log.flush();
+	}
 
-    public CompareFilesDialog(SwingLogPanel log, String binary, List<OurSyntaxWidget> list) {
-        this.log = log;
-        this.binary = binary;
-        this.tabsList = list;
-        if (log != null && binary != null) {
-        }
-        initUI();
-    }
+	public static void logOnChange(final SwingLogPanel log, final Pref<?>... prefs) {
+		if (log == null)
+			return;
+		for (final Pref<?> pref : prefs) {
+			pref.addChangeListener(new ChangeListener() {
 
-    protected final void initUI() {
+				@Override
+				public void stateChanged(ChangeEvent e) {
+					logPrefChanged(log, pref);
+				}
+			});
+		}
+	}
 
-        ArrayList<String> tabListNames = new ArrayList<String>();
-        for (OurSyntaxWidget tab : tabsList) {
-            tabListNames.add(tab.getFilename());
-        }
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
 
-        StringChoicePref tabNamesLeft = new StringChoicePref("LeftFile", "Left File", tabListNames);
-        StringChoicePref tabNamesRight = new StringChoicePref("RightFile", "Right File", tabListNames);
-
-        JButton compareButton = new JButton("Compare");
-
-        JPanel p = OurUtil.makeGrid(2, gbc().make(), mkCombo(tabNamesLeft), mkCombo(tabNamesRight), mkButton(compareButton));
-
-        this.tab = new JEditorPane();
-
-        //tab.addTab("", initEditorPane());
-        //tab.addTab("Solver", initSolverPane());
-        //tab.addTab("Miscellaneous", initMiscPane());
-
-        add(p);
-        //add(mkCombo(tabNamesRight));
-
-        setTitle("Compare Alloy Models");
-        pack();
-        setSize(getSize().width + 5, getSize().height + 5);
-        setResizable(false);
-        setLocationRelativeTo(null);
-        setAlwaysOnTop(false);
-
-        compareButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ModuleMerger m = new ModuleMerger(tabNamesLeft.get(), tabNamesRight.get());
-                //dispose();
-            }
-        });
-    }
-
-    @SuppressWarnings({
-                       "unchecked"
-    } )
-    protected <T> JPanel mkCombo(final ChoicePref<T> pref) {
-        JComboBox cb = make(new JComboBox(mkComboBoxModelFor(pref)));
-        pref2comp.put(pref, cb);
-        cb.setRenderer(new CBRenderer() {
-
-            @Override
-            protected Object render(Object value) {
-                return pref.renderValueShort((T) value);
-            }
-        });
-        return OurUtil.makeH(pref.title + ": ", cb);
-    }
-
-    protected <T> JPanel mkButton(JButton jb) {
-        return OurUtil.makeH(null, jb, null);
-
-    }
-
-    private <T> ComboBoxModel mkComboBoxModelFor(ChoicePref<T> pref) {
-        return new CBModel<T>(pref);
-    }
-
-    private <T extends JComponent> T make(T comp) {
-        return OurUtil.make(comp);
-    }
-
-
-    private Component makeTabPane(JPanel pane) {
-        JPanel p = new JPanel(new GridBagLayout());
-        // pane.setBorder(new OurBorder(true, true, true, true));
-        p.add(pane, gbc().pos(0, 0).fill(GridBagConstraints.BOTH).insets(new Insets(5, 5, 5, 5)).anchor(GridBagConstraints.NORTH).make());
-        p.add(new JLabel(), gbc().pos(0, 1).weighty(1).fill(GridBagConstraints.BOTH).make());
-        JPanel ans = OurUtil.make(p, new OurBorder(true, true, true, true));
-        return ans;
-    }
-
-    private GridBagConstraintsBuilder gbc() {
-        GridBagConstraintsBuilder ans = new GridBagConstraintsBuilder();
-        ans.anchor(GridBagConstraints.WEST).insets(new Insets(3, 3, 3, 3)).ipads(3, 3).fill(GridBagConstraints.BOTH);
-        return ans;
-    }
-
-    public static void logPrefChanged(SwingLogPanel log, Pref< ? > pref) {
-        if (log == null)
-            return;
-        log.log("Option ");
-        log.logBold(pref.title);
-        log.log(" changed to ");
-        log.logBold(pref.get() + "\n");
-        log.flush();
-    }
-
-    public static void logOnChange(final SwingLogPanel log, final Pref< ? >... prefs) {
-        if (log == null)
-            return;
-        for (final Pref< ? > pref : prefs) {
-            pref.addChangeListener(new ChangeListener() {
-
-                @Override
-                public void stateChanged(ChangeEvent e) {
-                    logPrefChanged(log, pref);
-                }
-            });
-        }
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                CompareFilesDialog sd = new CompareFilesDialog(null, null, null);
-                sd.setDefaultCloseOperation(EXIT_ON_CLOSE);
-                sd.setVisible(true);
-            }
-        });
-    }
+			@Override
+			public void run() {
+				CompareFilesDialog sd = new CompareFilesDialog(null, null, null, null);
+				sd.setDefaultCloseOperation(EXIT_ON_CLOSE);
+				sd.setVisible(true);
+			}
+		});
+	}
 
 }
