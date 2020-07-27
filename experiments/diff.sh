541bad00247af0e37d6d36927fa494b6274ec2ea
Jan Oliver Ringert <jor4@leicester.ac.uk>
2020-07-28 00:19:31 +0100

    improved command order to print timeout memory information

--- experiments/diff.sh
+++ experiments/diff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # compute diff statistics with a timeout of 10min for each pair of specs
-timeout -k 0s 10m /usr/bin/time -f "%M kB; %C" java -jar diff.jar $1 $2 $3 withPred 2>> memory.log
+#/usr/bin/time -f "%M kB; %C" timeout -k 0s 10m java -jar diff.jar $1 $2 $3 withPred 2>> memory.log
 # use the next line to produce statistics for single models (first parameter)
-#timeout -k 0s 10m /usr/bin/time -f "%M kB; %C" java -jar v1.jar $1 $2 $3 withPred 2>> memory.log
+/usr/bin/time -f "%M kB; %C" timeout -k 0s 10m java -jar v1.jar $1 $2 $3 withPred 2>> memory.log
